17a98f61a99507cb46d71f8c5ef2a2543c27204a
Marcos Pão <168776511+ukipaozin@users.noreply.github.com>
2024-06-27 17:31:28 -0300

    .

--- pong.js
+++ pong.js
@@ -1,9 +1,9 @@
 function setup(){
     //aqui vou criar minha "mesa"
-    createcanvas(600,400);
+    createCanvas(600,400);
 }
 
 function draw(){
     //aqui vou por a cor da "mesa"
-    backgound(255,255,240);
+    background(255,255,240);
 }
