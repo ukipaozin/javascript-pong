7acd7eba31cf0b29685830db999a8b04613a7f80
Marcos Pão <168776511+ukipaozin@users.noreply.github.com>
2024-07-25 17:14:35 -0300

    j

--- pong.js
+++ pong.js
@@ -1,8 +1,12 @@
+//apertar control f5 prra recaregar de vdd
 //aqui irei criar minha bolinha
 let xbolinha = 300
 let ybolinha = 200
 let tambolinha = 25
 
+//config bolinha eba uau que legal aaaaaaaaaaaa primeiro velocidade
+let xvelo = 6
+let yvelo = 2
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
@@ -10,7 +14,7 @@ function setup(){
 
 function draw(){
     //aqui vou por a cor da "mesa"
-    background(220,255,255);
+    background(200,255,255);
     criarbolinha(xbolinha,ybolinha,tambolinha)
 }
 
@@ -19,4 +23,9 @@ function criarbolinha(xbolinha,ybolinha,tambolinha){
     circle(xbolinha,ybolinha,tambolinha);
 
 
+}
+//funçao move bolinha
+function movebolinha(){
+    xbolinha = xbolinha + xbolinha
+    ybolinha = xvelo + ybolinha
 }
