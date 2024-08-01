fe01b1af92844d79cf13984276beaea9faba63c6
Marcos Pão <168776511+ukipaozin@users.noreply.github.com>
2024-08-01 14:07:14 -0300

    d

--- pong.js
+++ pong.js
@@ -1,41 +1,35 @@
 //apertar control f5 prra recaregar de vdd
-//aqui irei criar minha bolinha
-let xbolinha = 300
-let ybolinha = 200
-let tambolinha = 25
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
+let xvelocidadeBolinha = 40;
+let yvelocidadeBolinha = 6;
 
-//config bolinha eba uau que legal aaaaaaaaaaaa primeiro velocidade
-let xvelo = 4
-let yvelo = 7
 function setup(){
-    //aqui vou criar minha "mesa"
-    createCanvas(1000,700);
+    createCanvas(1200,800);
 }
 
 function draw(){
-    //aqui vou por a cor da "mesa"
-    background(140,255,255);
-    criarbolinha(xbolinha,ybolinha,tambolinha)
-    movebolinha();
+    background(18,128,50); 
+    criaBolinha(xBolinha, yBolinha, tamBolinha);
+    moveBolinha();
+    Borda();
 }
 
-//funçao bolinha
-function criarbolinha(xbolinha,ybolinha,tambolinha){
-    circle(xbolinha,ybolinha,tambolinha);
-
-
-}
-//funçao move bolinha
-function movebolinha(){
-    xbolinha = xvelo + xbolinha
-    ybolinha = yvelo + ybolinha
+function criaBolinha(xBolinha, yBolinha,tamBolinha){
+    circle (xBolinha,yBolinha,tamBolinha);
 }
 
-function borda(){
-    if (xbolinha > width || xbolinha < 0){
-        xvelo *= -1;
+function moveBolinha(){
+    xBolinha = xvelocidadeBolinha + xBolinha;
+    yBolinha = yvelocidadeBolinha + yBolinha;
+}
+
+function Borda(){
+    if (xBolinha > width || xBolinha < 0){
+        xvelocidadeBolinha *= -1;
     }
-    if (ybolinha > height || ybolinha < 0){
-        yvelo *= -1;
+    if (yBolinha > height || yBolinha < 0){
+        yvelocidadeBolinha *= -1;
     }
 }
